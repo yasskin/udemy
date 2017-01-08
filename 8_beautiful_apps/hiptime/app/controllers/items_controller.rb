be1b94095ca00cb883b5b451e28c774f0dbcbee3
Noah Yasskin <noahyasskin@gmail.com>
2017-01-07 22:42:08 -0800

    completed tutorial with styling

--- 8_beautiful_apps/hiptime/app/controllers/items_controller.rb
+++ 8_beautiful_apps/hiptime/app/controllers/items_controller.rb
@@ -2,18 +2,20 @@ class ItemsController < ApplicationController
   before_action :find_item, only: [:show, :edit, :update, :destroy]
 
   def index
-    @items = Item.all.order("created_at DESC")
+    if user_signed_in?
+      @items = Item.where(:user_id => current_user.id).order("created_at DESC")
+    end
   end
 
   def show
   end
 
   def new
-    @item = Item.new
+    @item = current_user.items.build
   end
 
   def create
-    @item = Item.new(item_params)
+    @item = current_user.items.build(item_params)
     if @item.save
       redirect_to root_path
     else
@@ -34,7 +36,13 @@ class ItemsController < ApplicationController
 
   def destroy
     @item.destroy
-    redirect_to root_path 
+    redirect_to root_path
+  end
+
+  def complete
+    @item = Item.find(params[:id])
+    @item.update_attribute(:completed_at, Time.now)
+    redirect_to root_path
   end
 
   private
