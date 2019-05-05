ba9d5f5b143c4bcc086264ce6b8ee850f26ebb59
Noah Yasskin <noahyasskin@gmail.com>
2019-05-04 21:31:52 -0700

    add user model request validation and some of the user registration

--- mern_stack/devconnector/routes/api/users.js
+++ mern_stack/devconnector/routes/api/users.js
@@ -1,9 +1,69 @@
 const express = require('express');
 const router = express.Router();
+const gravatar = require('gravatar');
+const { check, validationResult } = require('express-validator/check');
+const bcrypt = require('bcryptjs');
+const User = require('../../models/User');
 
-// @route   GET api/users
-// @desc    Test route
+// @route   POST api/users
+// @desc    Register user
 // @access  Public
-router.get('/', (req, res) => res.send('User route'));
+router.post(
+  '/',
+  [
+    check('name', 'Name is required')
+      .not()
+      .isEmpty(),
+    check('email', 'Please include a valid email').isEmail(),
+    check(
+      'password',
+      'Please eneter a password with six or more characters'
+    ).isLength({ min: 6 })
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { name, email, password } = req.body;
+
+    try {
+      // See if the user exists
+      let user = await User.findOne({ email });
+      // If exists, send back an error
+      if (user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'User already exists' }] });
+      }
+      // Get users gravatar
+      const avatar = gravatar.url(email, {
+        s: '200',
+        r: 'pg',
+        d: 'mm'
+      });
+      user = new User({
+        name,
+        email,
+        avatar,
+        password
+      });
+
+      // Encrypt password using bcrypt
+      const salt = await bcrypt.genSalt(10);
+
+      user.password = await bcrypt.hash(password, salt);
+
+      await user.save();
+
+      // Return jsonwebtoken
+      res.send('User registered');
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server error');
+    }
+  }
+);
 
 module.exports = router;
--- mern_stack/devconnector/server.js
+++ mern_stack/devconnector/server.js
@@ -7,7 +7,7 @@ const app = express();
 connectDB();
 
 // Init middleware
-//app.use(express.json({ extended: false }));
+app.use(express.json({ extended: false }));
 
 app.get('/', (req, res) => res.send('API Running'));
 
