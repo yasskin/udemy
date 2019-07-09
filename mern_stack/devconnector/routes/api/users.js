86ddbd417d81db6a6b98b5c75457c97c0f3f1d70
Noah Yasskin <noahyasskin@gmail.com>
2019-07-08 19:14:16 -0700

    add python django starter project

--- mern_stack/devconnector/routes/api/users.js
+++ mern_stack/devconnector/routes/api/users.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
-const { check, validationResult } = require('express-validator/check');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+
+const { check, validationResult } = require('express-validator/check');
+
 const User = require('../../models/User');
 
 // @route   POST api/users
@@ -43,6 +47,7 @@ router.post(
         r: 'pg',
         d: 'mm'
       });
+
       user = new User({
         name,
         email,
@@ -57,8 +62,21 @@ router.post(
 
       await user.save();
 
-      // Return jsonwebtoken
-      res.send('User registered');
+      const payload = {
+        user: {
+          id: user.id
+        }
+      };
+
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
     } catch (err) {
       console.error(err.message);
       res.status(500).send('Server error');
