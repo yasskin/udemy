5c48f62e0a966d06f714d5bc8beff02e9849beee
Noah Yasskin <noahyasskin@gmail.com>
2019-07-08 21:22:05 -0700

    add python dictionaries and tuples exercises

--- python_django/python_sandbox/dictionaries.py
+++ python_django/python_sandbox/dictionaries.py
@@ -1,1 +1,54 @@
 # A Dictionary is a collection which is unordered, changeable and indexed. No duplicate members.
+
+# Simple dictionary
+person = {
+    'first_name': 'John',
+    'last_name': 'Doe',
+    'age': 30
+}
+
+# Using a constructor
+'''
+person = dict(
+    first_name='John',
+    last_name='Doe',
+    age=30
+)
+'''
+
+# Access value
+print(person['first_name'])
+print(person.get('last_name'))
+
+# Add key/value
+person['phone'] = '555-555-5555'
+
+# Get keys
+print(person.keys())
+
+# Get items
+print(person.items())
+
+# Make a copy
+person2 = person.copy()
+person2['city'] = 'Boston'
+
+
+# Remove item
+del(person['age'])
+person.pop('phone')
+
+# Get length
+print(len(person2))
+
+# Clear
+person.clear()
+
+
+# List of dict
+people = [
+    {'name': 'Martha', 'age': 40},
+    {'name': 'Bob', 'age': 20}
+]
+
+print(people[1]['name'])
--- python_django/python_sandbox/tuples_sets.py
+++ python_django/python_sandbox/tuples_sets.py
@@ -1,5 +1,44 @@
 # A Tuple is a collection which is ordered and unchangeable. Allows duplicate members.
- 
 
+# Simple tuple
+fruit_tuple = ('Apple', 'Orange', 'Mango')
+
+# Using constructor
+# fruit_tuple = tuple(('Apple', 'Orange', 'Mango'))
+
+# Get single value
+# print(fruit_tuple[1])
+
+# Can not change value
+# fruit_tuple[1] = 'Grape' - does not support item assignment
+
+# Tuples with one value should have trailing comma
+fruit_tuple_2 = ('Apple',)
+
+del fruit_tuple_2
+
+# Get length of tupe
+print(len(fruit_tuple))
 
 # A Set is a collection which is unordered and unindexed. No duplicate members.
+
+# Create set
+fruit_set = {'Apple', 'Orange', 'Mango', 'Apple'}
+
+# Check if in set
+print('Apple' in fruit_set)
+print('Apples' in fruit_set)
+
+# Add to set
+fruit_set.add('Grape')
+
+# Remove from set
+fruit_set.remove('Grape')
+
+# Clear set
+fruit_set.clear()
+
+# Delete set
+del fruit_set
+
+print(fruit_set)
