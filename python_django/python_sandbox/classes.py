627e0e0018794059582a1f11479fdcd2f76ab80c
Noah Yasskin <noahyasskin@gmail.com>
2019-07-09 20:44:46 -0700

    add modules, classes, files and JSON coursework

--- python_django/python_sandbox/classes.py
+++ python_django/python_sandbox/classes.py
@@ -1,2 +1,56 @@
 # A class is like a blueprint for creating objects. An object has properties and methods(functions) associated with it. Almost everything in Python is an object
 
+# Create class
+
+
+class User:
+    # Constructor
+    def __init__(self, name, email, age):
+        self.name = name
+        self.email = email
+        self.age = age
+
+    def greeting(self):
+        return f'My name is {self.name} and I am {self.age}.'
+
+    def has_birthday(self):
+        self.age += 1
+
+
+# Init user object
+brad = User('Brad Traversy', 'brad@gmail.com', 37)
+janet = User('Janet Williams', 'janet@gmail.com', 17)
+
+# # Edit property
+# brad.age = 38
+
+# brad.has_birthday()
+
+# print(brad.age)
+
+# # Call method
+# print(janet.greeting())
+
+# Customer class
+
+
+class Customer(User):
+    # Constructor
+    def __init__(self, name, email, age):
+        self.name = name
+        self.email = email
+        self.age = age
+        self.balance = 0
+
+    def set_balance(self, balance):
+        self.balance = balance
+
+    def greeting(self):
+        return f'My name is {self.name} and I am {self.age} and I owe a balance of {self.balance}.'
+
+ # Init customer
+john = Customer('John Doe', 'john@gmail.com', 40)
+
+john.set_balance(500)
+
+print(john.greeting())
--- python_django/python_sandbox/files.py
+++ python_django/python_sandbox/files.py
@@ -1,1 +1,24 @@
 # Python has functions for creating, reading, updating, and deleting files.
+
+# Open a file
+myFile = open('myfile.txt', 'w')
+
+# Get info on file
+print('Name: ', myFile.name)
+print('Is closed: ', myFile.closed)
+print('Opening mode: ', myFile.mode)
+
+# Write to file
+myFile.write('I love Python')
+myFile.write(' and JavaScript.')
+myFile.close()
+
+# Append to file
+myFile = open('myfile.txt', 'a')
+myFile.write(' I also like PHP.')
+myFile.close()
+
+# Read from file
+myFile = open('myfile.txt', 'r+')
+text = myFile.read(10)
+print(text)
