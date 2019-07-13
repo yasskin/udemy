1f5f9dfab4174109550dc6b2fb372b73c168f114
Noah Yasskin <noahyasskin@gmail.com>
2019-07-13 15:14:31 -0700

    initial commit

---
+++ python_django/btre_project/venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/yasskin/Documents/dev/udemy/python_django/btre_project/venv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
