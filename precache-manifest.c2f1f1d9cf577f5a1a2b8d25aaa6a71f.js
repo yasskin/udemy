1b9372701e8f9f44e941ee6f5dba231322daf463
Noah Yasskin <noahyasskin@gmail.com>
2019-09-29 13:33:42 -0700

    Updates

---
+++ precache-manifest.c2f1f1d9cf577f5a1a2b8d25aaa6a71f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75a01f500ca1545c99c17cc809ea5d77",
+    "url": "/github.io/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "78308b088d03f565fe9d",
+    "url": "/github.io/monsters-rolodex/static/css/main.b02761f1.chunk.css"
+  },
+  {
+    "revision": "46012db1d69cedf8d1ab",
+    "url": "/github.io/monsters-rolodex/static/js/2.3d8af1b9.chunk.js"
+  },
+  {
+    "revision": "78308b088d03f565fe9d",
+    "url": "/github.io/monsters-rolodex/static/js/main.467f3a95.chunk.js"
+  },
+  {
+    "revision": "61d0f9e906b9767b8a50",
+    "url": "/github.io/monsters-rolodex/static/js/runtime-main.5155ecbc.js"
+  }
+]);
