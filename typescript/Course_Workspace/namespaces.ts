575bce00f9c323f6a57e27d9fdbf4c0d741a919c
Noah Yasskin <noahyasskin@gmail.com>
2019-11-03 14:12:34 -0800

    add modules and namespaces coursework

---
+++ typescript/Course_Workspace/namespaces.ts
@@ -0,0 +1,10 @@
+/// <reference path="circleMath.ts" />
+/// <reference path="rectangleMath.ts" />
+
+import CircleMath = MyMath.Circle;
+
+//const PI = 2.99;
+
+console.log(MyMath.calculateRectangle(10, 20));
+console.log(CircleMath.calculateCircumference(3));
+console.log(PI);
