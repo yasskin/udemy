d173a50816431c093a8b6b9037b1eab528bf6450
Noah Yasskin <noahyasskin@gmail.com>
2019-11-15 12:11:25 -0800

    add typescript complete guide course

---
+++ typescript_complete/fetchjson/index.js
@@ -0,0 +1,8 @@
+"use strict";
+// make a network request to fetch some json
+exports.__esModule = true;
+var axios_1 = require("axios");
+var url = 'http://jsonplaceholder.typicode.com/todos/1';
+axios_1["default"].get(url).then(function (response) {
+    console.log(response.data);
+});
