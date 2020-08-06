2919e73f8d424a14f523aee254e9a885a636f84b
Noah Yasskin <noahyasskin@gmail.com>
2020-08-06 12:04:39 -0700

    add Jest React testing coursework

--- react_testing/click-counter/src/App.js
+++ react_testing/click-counter/src/App.js
@@ -2,10 +2,35 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      counter: 0,
+    };
+  }
+
+  count = (num) => {
+    if (this.state.counter > 0) {
+      this.setState({
+        counter: this.state.counter + num,
+      });
+    }
+  };
+
   render() {
     return (
       <div data-test='component-app'>
-        <h1 className='App'>App</h1>
+        <h1 data-test='counter-display'>
+          The counter is currently {this.state.counter}
+          {this.state.counter === 0 && <h2>The counter cannot go below 0</h2>}
+        </h1>
+        <button data-test='increment-button' onClick={() => this.count(1)}>
+          Increment counter
+        </button>
+        <button data-test='decrement-button' onClick={() => this.count(-1)}>
+          Decrement counter
+        </button>
       </div>
     );
   }
