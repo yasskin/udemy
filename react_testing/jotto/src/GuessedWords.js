6dc0a476c131520b8406e4f43e7d2dccaefed81b
Noah Yasskin <noahyasskin@gmail.com>
2020-08-06 20:38:33 -0700

    add some Jest testing

--- react_testing/jotto/src/GuessedWords.js
+++ react_testing/jotto/src/GuessedWords.js
@@ -7,6 +7,30 @@ const GuessedWords = (props) => {
     contents = (
       <span data-test='guess-instructions'>Try to guess the secret word!</span>
     );
+  } else {
+    const guessedWordsRows = props.guessedWords.map((word, index) => (
+      <tr data-test='guessed-word' key={index}>
+        <td>{word.guessedWord}</td>
+        <td>{word.letterMatchCount}</td>
+      </tr>
+    ));
+    contents = (
+      <div data-test='guessed-words'>
+        <h3>Guessed Words</h3>
+        <table className='table table-sm'>
+          <thead className='thead-light'>
+            <tr>
+              <th>Guess</th>
+              <th>Matching Letters</th>
+            </tr>
+            <tbody>
+              {guessedWordsRows}
+              <tr></tr>
+            </tbody>
+          </thead>
+        </table>
+      </div>
+    );
   }
   return <div data-test='component-guessed-words'>{contents}</div>;
 };
